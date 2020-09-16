f6f3e857d1c223409e156d1f90f56d2ab74c0a9a
paikung <sumer_cup@hotmail.com>
2020-09-16 13:38:11 +0700

    JS-learn

---
+++ Example.js
@@ -0,0 +1,17 @@
+let name = "John"
+name = "JOKE"
+let test = "Hellow"
+name = `${test} B`
+
+let arr =['apple','banana','kiwi']
+console.log("arr: ", arr[0])
+
+let person = {
+    name: 'JOKE',
+    age: 15,
+    gender:'male'
+}
+console.log(person.name)
+
+//object
+
