b5bdc1f24b48049a8ea2318018ed6aa6d3569d05
paikung <sumer_cup@hotmail.com>
2020-09-16 13:47:53 +0700

    Create Quiz.js

---
+++ Quiz.js
@@ -0,0 +1,8 @@
+let Dataperson ={
+    firstName: "John",
+    lastName: "Doe",
+    age: 50,
+    myhobbies: ["playgame","playfootball"],
+    Phonenumber: "08922123301"
+}
+console.log(Dataperson)
