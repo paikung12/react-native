ab660592e081dca946fe8f7ea4e6695e57898aba
paikung <sumer_cup@hotmail.com>
2020-09-16 15:01:01 +0700

    Update Quiz2.js

--- Quiz2.js
+++ Quiz2.js
@@ -12,5 +12,5 @@ for(let i = 1 ; i<= 20; i+=1) {
 let result = [-1,2,3,56,6,-93,0,4].map(el => el*2)
 console.log(result)
 //4
-let result2 = [-1,2,3,56,6,-93,0,4].filter(el => el/2 === 0)
+let result2 = [-1,2,3,56,6,-93,0,4].filter(el => el%2 === 0)
 console.log(result2)
